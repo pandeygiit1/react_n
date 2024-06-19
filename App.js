41b8831fd59c94a6a60d8f1d9c8faeeafdc2a53f
rohit pandey <rohitpandey10102000@gmail.com>
2024-06-19 23:04:58 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,9 @@
+//const heading = React.createElement("h1",{id:"heading"},"Hello world from react");
+
+const parent = React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"i am h1 tag"),React.createElement("h1",{},"i am h2 tag")])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+  
